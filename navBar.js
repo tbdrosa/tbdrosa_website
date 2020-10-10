ecd84f4f4ec7615467a1cff2e7f6bb41810a3758
tbdrosa <tbdrosa@gmail.com>
2020-10-10 19:44:25 -0400

    created ~navigation bar function to toggle~ (navBar.js)

--- navBar.js
+++ navBar.js
@@ -1,1 +1,7 @@
+const toggleHamburger = document.getElementsByClassName('toggle-hamburger')[0]
 
+const navbarLinks = document.getElementsByClassName('navBarLinks')[0]
+
+toggleHamburger.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
